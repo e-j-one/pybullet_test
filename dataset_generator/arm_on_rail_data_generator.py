31b54e0d436630a47d72c234db8625e74a06b6a7
jwl <jwlee2843@gmail.com>
2024-08-31 03:08:42 +0900

    [feat] add data generator

--- dataset_generator/arm_on_rail_data_generator.py
+++ dataset_generator/arm_on_rail_data_generator.py
@@ -1,7 +1,12 @@
 from typing import List, Tuple
 import numpy as np
 
+import pybullet as p
+import pybullet_data
+
+import utils.bullet_obj_utils as bullet_obj_utils
 import utils.geometry_utils as geometry_utils
+from utils.collision_utils import get_check_collision_fn
 from utils.types import RobotState
 
 
@@ -27,12 +32,13 @@ class ArmOnRailDataGenerator:
         rail_length: float,
         num_obstacles: int,
         min_state_cost_threshold: float = 0.5,
-        min_r_obstacle: float = 0.2,
+        min_r_obstacle: float = 0.4,
         max_r_obstacle: float = 1.0,
         min_phi_obstacle: float = 0.0,
         max_phi_obstacle: float = np.pi,
         min_length_obstacle: float = 0.1,
-        max_length_obstacle: float = 0.5,
+        max_length_obstacle: float = 0.4,
+        robot_urdf_path="urdf/ur5.urdf",
     ):
         self.rail_length = rail_length
         self.num_obstacles = num_obstacles
@@ -44,6 +50,7 @@ class ArmOnRailDataGenerator:
         self.max_phi_obstacle = max_phi_obstacle
         self.min_length_obstacle = min_length_obstacle
         self.max_length_obstacle = max_length_obstacle
+        self.robot_urdf_path = robot_urdf_path
 
         self.num_joints = 6
         self.num_max_state_tries = int(1e5)
@@ -56,7 +63,10 @@ class ArmOnRailDataGenerator:
         state = []
         for i in range(self.num_joints + 1):
             if i == 0:
-                state.append(np.random.uniform(0, self.rail_length))
+                state.append(np.random.uniform(0.0, self.rail_length))
+                continue
+            if i == 2:
+                state.append(np.random.uniform(-np.pi, 0.0))
                 continue
             state.append(np.random.uniform(-np.pi, np.pi))
         return tuple(state)
@@ -68,39 +78,100 @@ class ArmOnRailDataGenerator:
         List[Tuple[float, float, float]],
         List[Tuple[float, float, float]],
     ]:
+        """
+        Sample a new map with start, goal, rail length, obstacles
+        1. Sample start position until it is collision free.
+        2. Sample goal position until it is collision free and cost > min_state_cost_threshold.
+        3. Sample obstacles until collision free.
+        """
+
+        p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        self.plane_uid = p.loadURDF("plane.urdf")
+        self.robot_uid = bullet_obj_utils.spawn_robot(urdf_path=self.robot_urdf_path)
+        obstacle_uids = [self.plane_uid]
+        non_fixed_joint_ids = bullet_obj_utils.get_non_fixed_joints(self.robot_uid)
+
+        check_collision = get_check_collision_fn(
+            robot_uid=self.robot_uid,
+            joint_ids=non_fixed_joint_ids,
+            obstacles=obstacle_uids,
+            rail_length=self.rail_length,
+        )
+
         num_tries = 0
         while num_tries < self.num_max_state_tries:
             start_state = self._sample_robot_state()
-            goal_state = self._sample_robot_state()
+            if not check_collision(start_state):
+                break
+            num_tries += 1
 
+        print("start_state found after ", num_tries, " tries ", start_state)
+
+        while num_tries < self.num_max_state_tries:
+            goal_state = self._sample_robot_state()
             if (
-                geometry_utils.get_cost_between_states(start_state, goal_state)
+                not check_collision(goal_state)
+                and geometry_utils.get_cost_between_states(start_state, goal_state)
                 > self.min_state_cost_threshold
             ):
                 break
             num_tries += 1
 
+        print("goal_state found after ", num_tries, " tries ", goal_state)
+
         obstacle_positions = []
         obstacle_dimensions = []
 
-        for _ in range(self.num_obstacles):
-            r_obstacle = np.random.uniform(self.min_r_obstacle, self.max_r_obstacle)
-            phi_obstacle = np.random.uniform(
-                self.min_phi_obstacle, self.max_phi_obstacle
-            )
-            x_obstacle = np.random.uniform(0, self.rail_length)
-            y_obstacle = r_obstacle * np.cos(phi_obstacle)
-            z_obstacle = r_obstacle * np.sin(phi_obstacle)
-            width_obstacle, length_obstacle, height_obstacle = (
-                np.random.uniform(self.min_length_obstacle, self.max_length_obstacle),
-                np.random.uniform(self.min_length_obstacle, self.max_length_obstacle),
-                np.random.uniform(self.min_length_obstacle, self.max_length_obstacle),
+        while num_tries < self.num_max_state_tries:
+            obstacle_positions = []
+            obstacle_dimensions = []
+            for _ in range(self.num_obstacles):
+                r_obstacle = np.random.uniform(self.min_r_obstacle, self.max_r_obstacle)
+                phi_obstacle = np.random.uniform(
+                    self.min_phi_obstacle, self.max_phi_obstacle
+                )
+                x_obstacle = np.random.uniform(0, self.rail_length)
+                y_obstacle = r_obstacle * np.cos(phi_obstacle)
+                z_obstacle = r_obstacle * np.sin(phi_obstacle)
+                width_obstacle, length_obstacle, height_obstacle = (
+                    np.random.uniform(
+                        self.min_length_obstacle, self.max_length_obstacle
+                    ),
+                    np.random.uniform(
+                        self.min_length_obstacle, self.max_length_obstacle
+                    ),
+                    np.random.uniform(
+                        self.min_length_obstacle, self.max_length_obstacle
+                    ),
+                )
+
+                obstacle_positions.append([x_obstacle, y_obstacle, z_obstacle])
+                obstacle_dimensions.append(
+                    [width_obstacle, length_obstacle, height_obstacle]
+                )
+
+            spawned_obstacle_uids = bullet_obj_utils.spawn_obstacles(
+                obstacle_positions, obstacle_dimensions
             )
 
-            obstacle_positions.append([x_obstacle, y_obstacle, z_obstacle])
-            obstacle_dimensions.append(
-                [width_obstacle, length_obstacle, height_obstacle]
+            check_collision = get_check_collision_fn(
+                robot_uid=self.robot_uid,
+                joint_ids=non_fixed_joint_ids,
+                obstacles=spawned_obstacle_uids,
+                rail_length=self.rail_length,
             )
+
+            if not check_collision(start_state) and not check_collision(goal_state):
+                break
+            for obstacle_uid in spawned_obstacle_uids:
+                p.removeBody(obstacle_uid)
+            num_tries += 1
+        print(
+            "obstacle_positions found after ", num_tries, " tries ", obstacle_positions
+        )
+
+        p.disconnect()
 
         return (
             start_state,
--- generate_dataset.py
+++ generate_dataset.py
@@ -5,16 +5,21 @@ import numpy as np
 
 from dataset_generator.arm_on_rail_data_generator import ArmOnRailDataGenerator
 
-from planners.rrt_planner import RrtPlanner
 from planners.rrt_star_planner import RrtStarPlanner
-from env.arm_on_rail_env import ArmOnRailEnv
+from arm_on_rail_env.arm_on_rail_env import ArmOnRailEnv
 import utils.plot_utils as plot_utils
 
-SEED = 0
+SEED = 1
 np.random.seed(SEED)
 
 
-def generate_dataset():
+def generate_dataset(
+    num_samples=2000,
+    rail_length=3.0,
+    num_obstacles=10,
+    max_iter=1000000,
+    dataset_dir="datasets/train/",
+):
     planner_config = {
         "max_iter": 80000,
         "collision_check_step_size": 0.04,
@@ -25,80 +30,84 @@ def generate_dataset():
         # rrt star
         "near_radius": 0.648,
     }
+    robot_urdf_path = "urdf/ur5.urdf"
 
-    # =============================== get env ===============================
+    num_samples_collected = 0
+
     arm_on_rail_env = ArmOnRailEnv()
-
-    # =============================== get env ===============================
     data_generator = ArmOnRailDataGenerator()
     data_generator.set_config(
-        rail_length=3.0,
-        num_obstacles=4,
-    )
-    start_state, goal_state, rail_length, obstacle_positions, obstacle_dimensions = (
-        data_generator.sample_new_map_data()
-    )
-
-    # =============================== get collision checker ===============================
-    arm_on_rail_env.reset_env(
-        robot_urdf_path="urdf/ur5.urdf",
         rail_length=rail_length,
-        start_state=start_state,
-        goal_state=goal_state,
-        obstacle_positions=obstacle_positions,
-        obstacle_dimensions=obstacle_dimensions,
+        num_obstacles=num_obstacles,
     )
 
-    if arm_on_rail_env.is_initial_state_in_collision():
-        print("Initial state is in collision")
-        return
+    while num_samples_collected < num_samples:
+        (
+            start_state,
+            goal_state,
+            rail_length,
+            obstacle_positions,
+            obstacle_dimensions,
+        ) = data_generator.sample_new_map_data()
 
-    # =============================== plan path ===============================
-
-    robot_uid = arm_on_rail_env.get_robot_uid()
-    non_fixed_joint_ids = arm_on_rail_env.get_non_fixed_joint_ids()
-    robot_state_ranges = arm_on_rail_env.get_robot_state_ranges()
-    check_collision = arm_on_rail_env.get_check_collision_fn()
-
-    motion_planner = RrtStarPlanner(
-        max_iter=planner_config["max_iter"],
-        joint_ids=non_fixed_joint_ids,
-        robot_state_ranges=robot_state_ranges,
-        collision_check_step_size=planner_config["collision_check_step_size"],
-        goal_reached_threshold=planner_config["goal_reached_threshold"],
-        drive_dist=planner_config["drive_dist"],
-        goal_sample_rate=planner_config["goal_sample_rate"],
-        near_radius=planner_config["near_radius"],
-    )
-
-    motion_planner.set_env(
-        robot_uid=robot_uid,
-        start_state=start_state,
-        goal_state=goal_state,
-        obstacle_positions=obstacle_positions,
-        obstacle_dimensions=obstacle_dimensions,
-        check_collision_fn=check_collision,
-    )
-
-    start_time = time.time()
-    sbmp_path, num_samples = motion_planner.plan_path()
-    print("time: ", time.time() - start_time)
-
-    print("n sample: ", num_samples)
-
-    # =============================== plot path ===============================
-
-    if sbmp_path is not None:
-        plot_utils.plot_path_forever(
-            path=sbmp_path,
-            joint_ids=non_fixed_joint_ids,
-            robot_uid=robot_uid,
-            plot_end_effector_poses=True,
+        arm_on_rail_env.reset_env(
+            robot_urdf_path=robot_urdf_path,
+            rail_length=rail_length,
+            start_state=start_state,
+            goal_state=goal_state,
+            obstacle_positions=obstacle_positions,
+            obstacle_dimensions=obstacle_dimensions,
         )
 
-    while p.isConnected():
-        pass
-        # p.stepSimulation()
+        if arm_on_rail_env.is_initial_state_in_collision():
+            print("============ Initial state is in collision ============")
+            print("============ robot uid: ", arm_on_rail_env.get_robot_uid())
+            print("============ obstc uid: ", arm_on_rail_env.get_obstacle_uids())
+            arm_on_rail_env.close_sim_env()
+            continue
+
+        robot_uid = arm_on_rail_env.get_robot_uid()
+        non_fixed_joint_ids = arm_on_rail_env.get_non_fixed_joint_ids()
+        robot_state_ranges = arm_on_rail_env.get_robot_state_ranges()
+        check_collision = arm_on_rail_env.get_check_collision_fn()
+
+        # =============================== plan path ===============================
+        motion_planner = RrtStarPlanner(
+            max_iter=planner_config["max_iter"],
+            joint_ids=non_fixed_joint_ids,
+            robot_state_ranges=robot_state_ranges,
+            collision_check_step_size=planner_config["collision_check_step_size"],
+            goal_reached_threshold=planner_config["goal_reached_threshold"],
+            drive_dist=planner_config["drive_dist"],
+            goal_sample_rate=planner_config["goal_sample_rate"],
+            near_radius=planner_config["near_radius"],
+        )
+        motion_planner.set_env(
+            robot_uid=robot_uid,
+            start_state=start_state,
+            goal_state=goal_state,
+            obstacle_positions=obstacle_positions,
+            obstacle_dimensions=obstacle_dimensions,
+            check_collision_fn=check_collision,
+        )
+
+        start_time = time.time()
+        sbmp_path, num_samples = motion_planner.plan_path()
+        print("time: ", time.time() - start_time)
+
+        print("n sample: ", num_samples)
+
+        if sbmp_path is not None:
+            plot_utils.plot_path_forever(
+                path=sbmp_path,
+                joint_ids=non_fixed_joint_ids,
+                robot_uid=robot_uid,
+                plot_end_effector_poses=True,
+            )
+
+        while p.isConnected():
+            pass
+            # p.stepSimulation()
 
 
 if __name__ == "__main__":
